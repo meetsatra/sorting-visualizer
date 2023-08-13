d72786fa1d85fd82266a5f562aaa070e565fb75f
Meet Satra <meetvsatra@gmail.com>
2023-08-13 23:23:33 +0530

    basic frontend with style and hover effect on buttons

--- sorting-visualizer/src/App.js
+++ sorting-visualizer/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <SortingVisualizer></SortingVisualizer>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
